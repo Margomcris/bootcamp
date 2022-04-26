50ea792e2abde27986b46340a823798b29b0078f
margomcris <margomcris@gmail.com>
2022-04-26 12:29:16 +0200

    Proyectos

---
+++ config/src/test/java/com/nttdataconfig/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdataconfig.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
